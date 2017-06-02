50de7c1207e51370bb99490372aae13d4944e33b
my88899 <haojiutao@gmail.com>
2017-06-02 09:54:33 +0800

    create class Person, Student

--- main/person.js
+++ main/person.js
@@ -1,2 +1,9 @@
 // Write your code here
+class Person{
+	constructor(name, age){	
+		this.name = name;
+		this.age = age;
+	}
+	introduce(){return ("My name is " + this.name + ". I am " + this.age + " years old.");}
+}
 module.exports = Person
